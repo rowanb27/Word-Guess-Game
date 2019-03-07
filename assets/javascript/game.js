13d57cee80ba78aaf2f2d380959e6ac7f47dff93
Robert Rowan <rowanb27@yahoo.com>
2019-03-07 11:28:45 -0600

    add beginning code of js

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var wordGameLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+var wins = 0;
+var losses = 0;
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var userChoiceText = document.getElementById("userchoice-text");
